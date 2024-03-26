4d7ac8559358670ba4b6973cc3ad110cd71028ca
Arnab <arnabchakrabortybd@gmail.com>
2024-03-26 22:24:50 +0600

    Commands change

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -1,12 +1,13 @@
 
-package javaapplication1;
-
 
 public class JavaApplication1 {
 
   
     public static void main(String[] args) {
-        // TODO code application logic here
+       
+
+
+
         System.out.print("Arnab you are the best");
     }
     
